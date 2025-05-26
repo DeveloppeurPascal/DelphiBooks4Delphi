e72b274067e85370f8574d6f7c0a66765e5e5d30
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 15:50:43 +0200

    updated C2PP comments

--- samples/FMX-Books-List-Sample/FMXBooksListSample.dpr
+++ samples/FMX-Books-List-Sample/FMXBooksListSample.dpr
@@ -1,39 +1,39 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Delphi Books API client for Delphi
-///
-/// Copyright 2020-2025 Patrick PREMARTIN under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Client library and samples programs showing how to use Delphi-Books.com
-/// open data API in Delphi and Pascal languages.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://delphibooks4delphi.developpeur-pascal.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/DelphiBooks4Delphi
-///
-/// ***************************************************************************
-/// File last update : 2025-02-05T20:59:20.275+01:00
-/// Signature : e159b640f14b97429e23538b5fdbd6adebdc4bfc
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Delphi Books API client for Delphi
+
+  Copyright 2020-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Client library and samples programs showing how to use Delphi-Books.com
+  open data API in Delphi and Pascal languages.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://delphibooks4delphi.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/DelphiBooks4Delphi
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:03:50.267+01:00
+  Signature : 9bfb17b77193ccd308d83d4ccfb53b7375786f5c
+  ***************************************************************************
+*)
 
 program FMXBooksListSample;
 
